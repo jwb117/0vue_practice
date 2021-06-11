d529bac447e435d6678aed166e439342901a31a4
jwb117 <jwbjoo@naver.com>
2021-06-11 15:21:39 +0900

    all

---
+++ src/data.js
@@ -0,0 +1,50 @@
+export default [{
+    id : 0,
+    title: "야경의 도시 싱가포르 마리나베이",
+    image: "./assets/img02.png",
+    content: "싱가포르 마리나베이 야경 투어",
+    price: 20000
+    },
+    {
+    id : 1,
+    title: "마리나베이의 명물, 인피니티풀",
+    image: "./assets/img03.png",
+    content: "샌즈 호텔 인피티니풀에서 내려다 본 전경",
+    price: 25000
+    },
+    {
+    id : 2,
+    title: "리버사이드 강변 투어",
+    image: "./assets/img04.png",
+    content: "리버사이드에서 진행되는 싱가포르 투어",
+    price:30000
+    },
+    {
+    id : 3,
+    title: "리틀인디아 시내 투어",
+    image: "./assets/img05.png",
+    content: "부킷빈탕에서부터 시작하는 싱가포르 문화 체험",
+    price: 35000
+    },
+    {
+    id : 4,
+    title: "싱가포르 최고의 플라톤호텔",
+    image: "./assets/img06.png",
+    content: "5성급 최고 시설의 싱가포르 명물 호텔",
+    price: 330000
+    },
+    {
+    id : 5,
+    title: "싱가포르 번화가 오차드로드",
+    image: "./assets/img07.png",
+    content: "쇼핑과 먹거리가 함께하는 식도락 여행",
+    price: 40000
+    },
+    {
+    id : 6,
+    title: "특이하고 개성넘치는 차이나타운",
+    image: "./assets/img10.png",
+    content: "볼거리, 놀거리 가득한 차이나타운 투어",
+    price: 40000
+    }
+]
