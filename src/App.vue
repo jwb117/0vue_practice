89562e116794ed165b859c87e062b58decaaf1cf
jwb117 <jwbjoo@naver.com>
2021-06-14 14:11:22 +0900

    final

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,12 @@
 
   </div>
 </div> -->
-<modal v-bind:tour="tour" v-bind:tourView="tourView" v-bind:listNum="listNum" @modalClose="tourView=false"></modal>
+
+<div class="start" v-bind:class="{end:tourView}">
+  
+    <modal v-bind:tour="tour" v-bind:tourView="tourView" v-bind:listNum="listNum" @modalClose="tourView=false"></modal>
+  
+</div>
 
 
 </template>
@@ -63,4 +68,10 @@ img{width: 100%;}
 
 .black-bg{width: 100%; position: fixed; background: rgba(0,0,0,0.7); top:0; height: 100%; display: flex; justify-content: center; align-items:center;}
 .white-bg{width: 80%; background: #fff; border-radius: 10px; padding: 20px;}
+
+.start{opacity:0; transition:1s;}
+.start.end{opacity:1;}
+
+.close{opacity: 1; transition: 1s;}
+.close.end{opacity: 0;}
 </style>
